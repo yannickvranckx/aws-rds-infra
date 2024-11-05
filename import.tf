0e4ef06328d1f6a6520b316878f2f2539fb5b8ab
Yannick Vranckx <41998248+yannickvranckx@users.noreply.github.com>
2024-11-05 11:48:10 +0100

    Update import.tf

--- import.tf
+++ import.tf
@@ -1,12 +1,12 @@
 import {
-  to = aws_db_instance.automic_acceptance
+  to = aws_db_instance.rds_instance
   id = "db-euw3-wa-acc"
 }
 import {
-  to = aws_db_parameter_group.automic_acceptance
+  to = aws_db_parameter_group.rds_instance
   id = "db-euw3-wa-acc-parameters"
 }
 import {
-  to = aws_db_subnet_group.automic_acceptance
+  to = aws_db_subnet_group.rds_instance
   id = "euw3-rds-automic-acc-db-subnet"
 }
